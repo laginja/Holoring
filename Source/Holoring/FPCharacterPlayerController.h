30736b2bf1c541e5255864f2394acb3a0b3e619e
Smoki-PC\Smoki <matko.smoljan@gmail.com>
2019-01-21 22:49:19 +0100

    Overriden GameMode and reparented player controller class

---
+++ Source/Holoring/FPCharacterPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FPCharacterPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLORING_API AFPCharacterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
